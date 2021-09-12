e9a6122281216b9d5f0c347be390ef143e2b9084
Avinash0110 <avinash.mukkamula@zemosolabs.com>
2021-09-12 13:34:08 +0530

    child-parent

--- child-parent/src/components/BookCard.js
+++ child-parent/src/components/BookCard.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react'
-import BookDetails from './BookDetails'
-import Thumnail from './Thumnail'
-import Image from './Image'
+import React, { Component } from 'react';
+import book from './book.jpg';
+
+function BookDetails() {
+    return (
+        <div>
+            <h3 style={{color:"brown"}}>Book Name : Birds and Animals </h3>
+        </div>
+    )
+}
+
+function Image() {
+    return (
+        <div>
+            <img src={book} width="300" height="300" alt="book " />
+        </div>
+    )
+}
+
+function Thumnail() {
+    return (
+        <div>
+            <h1 style={{color:"blue"}}>Non social animals</h1>
+        </div>
+    )
+}
+
 
 class BookCard extends Component {
     render() {
-        // const mystyle={
-        //      borderStyle: "solid",
-        //     borderWidth: "10px",
-        //     borderColor: "indigo",
-        //     textAlign: "center"
-            
-            
-            
-        //     }
+       
         return (
             <div >
                 <Image />
